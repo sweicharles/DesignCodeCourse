2c63770663749459db6d4218dbb484cbb7d6c612
CharlesSwei <sweicharles@gmail.com>
2021-07-13 23:12:57 +1000

    Initial Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by charles swei on 13/7/21.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
