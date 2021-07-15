56ecedcb09937678082414ed36c6b13a4784a4d6
CharlesSwei <sweicharles@gmail.com>
2021-07-16 00:27:46 +1000

    Add the Asymmetric in and out transaction and delay. Add multiple cards with VStack

--- Shared/CoursesView.swift
+++ Shared/CoursesView.swift
@@ -14,22 +14,44 @@ struct CoursesView: View {
     
     var body: some View{
         ZStack {
-            CourseItem() // aways put the geometry effect before the frame, the isSource prop define which one is original, so the effect and make another one as target
-                .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
-                .frame(width: 335, height: 250, alignment: .center)
+            ScrollView {
+                VStack(spacing: 20) {
+                    CourseItem() // aways put the geometry effect before the frame, the isSource prop define which one is original, so the effect and make another one as target
+                        .matchedGeometryEffect(id: "Card", in: namespace, isSource: !show)
+                        .frame(width: 335, height: 250)
+                    
+                    CourseItem()
+                        .frame(width: 335, height: 250)
+                }
+                .frame(maxWidth:.infinity)
+            }
             if show {
                 ScrollView {
                     CourseItem()
                         .matchedGeometryEffect(id: "Card", in: namespace)
-                        .frame(height: 350)
-                    VStack{
+                        .frame(height: 300)
+                    VStack(spacing: 5){
                         ForEach(0 ..< 20) { item in
                             CourseRow()
                         }
                     }
                     .padding()
                 }
-                .transition(.opacity)
+                .background(Color("Background 1"))
+                .transition(
+                    // Apply transaction animaiton on opacity style with spring style
+                    // .asymmetric can let us decide what kinda a transition when get in and get out
+                    .asymmetric(
+                        insertion: AnyTransition
+                            .opacity
+                            .animation(
+                                Animation.spring()
+                                    .delay(0.3)),
+                        removal: AnyTransition
+                            .opacity
+                            .animation(.spring())
+                    )
+                )
                 .edgesIgnoringSafeArea(.all)
             }
             
